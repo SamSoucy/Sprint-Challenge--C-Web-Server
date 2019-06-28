37fe8a96ffe1fd21f7842e6a8424c5e274dbf4c0
Samsoucy <newf930@outlook.com>
2019-06-28 12:16:09 -0400

    constructed the HTTP request

--- src/client.c
+++ src/client.c
@@ -86,9 +86,16 @@ int send_request(int fd, char *hostname, char *port, char *path)
   char request[max_request_size];
   int rv;
 
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
+  int request_length = sprintf(request,
+    "GET /%s HTTP/1.1\n"
+    "Host: %s:%s\n"
+    "Connection: close\n\n",
+    path, hostname, port);
+
+  if (rv < 0){
+    perror("send");
+  }
+  rv = send(fd, request, request_length, 0);
 
   return 0;
 }

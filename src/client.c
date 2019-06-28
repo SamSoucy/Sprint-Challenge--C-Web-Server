feab3af679900ae3d48635fa5698c5d9fc512d17
Samsoucy <newf930@outlook.com>
2019-06-28 11:49:53 -0400

    Parsed the input URL

--- src/client.c
+++ src/client.c
@@ -36,18 +36,36 @@ urlinfo_t *parse_url(char *url)
 
   /*
     We can parse the input URL by doing the following:
-
-    1. Use strchr to find the first slash in the URL (this is assuming there is no http:// or https:// in the URL).
-    2. Set the path pointer to 1 character after the spot returned by strchr.
-    3. Overwrite the slash with a '\0' so that we are no longer considering anything after the slash.
-    4. Use strchr to find the first colon in the URL.
-    5. Set the port pointer to 1 character after the spot returned by strchr.
-    6. Overwrite the colon with a '\0' so that we are just left with the hostname.
   */
 
-  ///////////////////
-  // IMPLEMENT ME! //
-  ///////////////////
+ //check if no http:// or https:// in the URL
+if(strstr(url, "https://")){
+  hostname = strdup(url + 8);
+}  else if (strstr(url, "http://")){
+  hostname = strdup(url + 7);
+}  else {
+  hostname = strdup(url);
+}
+
+ //Use strchr to find the first slash in the URL (this is assuming there is no http:// or https:// in the URL)
+ //Set the path pointer to 1 character after the spot returned by strchr.
+ //Overwrite the slash with a '\0' so that we are no longer considering anything after the slash.
+
+  char *slash = strchr(hostname, "/");
+  path = slash + 1;
+  *slash = "\0";
+
+//Use strchr to find the first colon in the URL.
+//Set the port pointer to 1 character after the spot returned by strchr.
+//Overwrite the colon with a '\0' so that we are just left with the hostname.
+  
+  char *colon = strchr(hostname, ":");
+  port = colon + 1;
+  *colon = "\0";
+
+  urlinfo->hostname = hostname;
+  urlinfo->port = port;
+  urlinfo->path = path;
 
   return urlinfo;
 }
